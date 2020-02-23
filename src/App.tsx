a1adfb4f27c25be03d7f16c76d64f0a727bea9fa
Hoang Minh Thang <hoangminhthang21051995@gmail.com>
2020-02-23 23:40:54 +0700

    Test some grid css

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ function App() {
     return (
         <div className="App">
             <div className="grid--container">
-                <div className="item">A</div>
-                <div className="item">B</div>
+                <div className="item box--1">A</div>
+                <div className="item box--2">B</div>
                 <div className="item">C</div>
                 <div className="item">D</div>
                 <div className="item">E</div>
                 <div className="item">F</div>
                 <div className="item">G</div>
                 <div className="item">H</div>
-                <div className="item">I</div>
+                <div className="item box--3">I</div>
                 <div className="item">J</div>
+                <div className="item">K</div>
+                <div className="item">L</div>
+                <div className="item">M</div>
+                <div className="item">N</div>
+                <div className="item">oO</div>
             </div>
         </div>
     );
