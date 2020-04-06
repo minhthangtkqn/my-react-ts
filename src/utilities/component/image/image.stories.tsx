73a0ce24bf4390a75bdf3e878562d19ff208f832
Hoang Minh Thang <hoangminhthang21051995@gmail.com>
2020-04-07 00:55:45 +0700

    Add: Image component with loading effect

---
+++ src/utilities/component/image/image.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyImage from 'utilities/component/image';
+
+export default {
+    title: 'Image',
+    component: MyImage,
+};
+
+const imgUrl = 'https://cdn.vox-cdn.com/thumbor/WEfQUw-9v5ti4MQ_fi84t7Vd6OM=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/18980526/TuSimple_Self_Drving_Truck_4_copy.jpg';
+
+export const ToStorybook = () => <MyImage src={imgUrl}/>;
+
+ToStorybook.story = {
+    name: '1 - My Image',
+};
---
+++ src/utilities/component/image/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Spin } from 'antd';
+import noImgUrl from 'asset/images/s-placeholder.png';
+
+type Props = {
+    src: string
+};
+
+const MyImage: React.FC<Props> = (props) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [imageSrc, setImageSrc] = useState<string>('');
+
+    useEffect(() => {
+        var downloadingImage = new Image();
+
+        downloadingImage.onload = function () {
+            setIsLoading(false);
+            setImageSrc(props.src);
+        };
+
+        downloadingImage.onerror = function () {
+            setIsLoading(false);
+            setImageSrc(noImgUrl);
+        };
+
+        setIsLoading(true);
+        downloadingImage.src = props.src;
+
+        return () => {
+            downloadingImage.onload = function () { };
+            downloadingImage.onerror = function () { };
+        };
+    }, []);
+
+    const render = () => {
+        return (
+            <img src={imageSrc} width="300" height="300" style={{ background: '#CCC' }} />
+        );
+    };
+
+    return (
+        <div style={{ width: 300, height: 300 }}>
+            <Spin spinning={isLoading}>
+                {render()}
+            </Spin>
+        </div>
+    );
+}
+
+export default MyImage;
